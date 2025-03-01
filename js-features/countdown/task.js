3eb267687417059ec605ec864add5c314c44aad8
Olesya Dr <odryankova@yandex.ru>
2025-03-01 12:01:21 +0300

    второй коммит счетчика

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,9 @@
 const countdown = function() {
-    const score = document.getElementById(`timer`);
+    const score = document.getElementById('timer');
     score.textContent -= 1;
     if (score.textContent <= 0) {
       clearInterval(intervalId);
-      alert(`Вы победили в конкурсе!`);
+      alert('Вы победили в конкурсе!');
     }
   };
   const intervalId = setInterval(countdown, 1000);
